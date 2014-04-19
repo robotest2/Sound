4852cb9b5ba04356d0f13ba1472a261639de3cb1
Pr0Code <razershockz@gmail.com>
2014-04-19 11:21:51 -0400

    Update room.js

--- room.js
+++ room.js
@@ -12,7 +12,7 @@ API.sendChat("/em now live!");
 
 API.on(API.CHAT, function(data){
         if (data.message.indexOf('!help') === 0 && API.getUser(data.fromID).permission > -1 || API.getUser(data.fromID).permission < 10){
-        	API.moderateDeleteChat(data.chatID);
+        	API.moderateDeleteChat(data.message);
         	API.sendChat("/em Test successfull");
         }
 }
