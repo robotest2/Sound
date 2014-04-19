4f7b9f391be81011c9b80507625a040454dcd22b
Pr0Code <razershockz@gmail.com>
2014-04-19 19:54:35 -0400

    Update sound.js

--- sound.js
+++ sound.js
@@ -48,6 +48,23 @@ var askArray = [
 
 //Commands
 
+users = {}
+getUserByName = function(name) {
+    for(var i in API.getUsers()) {
+        if (API.getUsers()[i].username === name.trim()) return API.getUsers()[i].id;
+    }
+}
+dcLookup = function(id) {
+        if(typeof users[id] !== 'undefined') API.sendChat(API.getUser(id).username + ' disconnected ~' + Math.round((Date.now() - users[id].time) / 60000) + ' minutes ago at position ' + (users[id].index + 1));
+        else API.sendChat('I haven\'t seen that user disconnect!');
+}
+API.on(API.USER_LEAVE, function(data){
+        users[data.id] = { id: data.id, index: data.wlIndex, time: Date.now() }
+});
+API.on(API.CHAT, function(data){
+        if (data.message.indexOf('!dc') === 0 && API.getUser(data.fromID).permission > 1) dcLookup(getUserByName(data.message.substring(11)));
+});
+
 //User
 
     API.on(API.CHAT, function(data){
