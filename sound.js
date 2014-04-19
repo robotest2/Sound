286d894864824568ee73594e4bfac9e703a20c0c
Pr0Code <razershockz@gmail.com>
2014-04-19 19:16:51 -0400

    Update sound.js

--- sound.js
+++ sound.js
@@ -31,18 +31,15 @@ API.sendChat("/em now live!");
     API.on(API.CHAT, function(data){
     	if(data.message.indexOf('!link') === 0){
     		if(API.getMedia().format == 1){
-    			
     		API.moderateDeleteChat(data.chatID);
     		API.sendChat("/em [" + data.from + "] Link to current song: http://youtu.be/" + API.getMedia().cid);
     		}else{
-    		if(data.message.indexOf('link') === 0){
     			var id = API.getMedia().cid;
     			SC.get('/tracks', { ids: id,}, function(tracks) {
     				API.sendChat("/em [" + data.from + "] Link to current song: " + tracks[0].permalink_url);
     			});
     		}
     	}
-    });
     });
     
     //Put more here soon
