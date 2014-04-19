bcd5942504f982d3d25e6710d80dc22e40b5bb4c
Pr0Code <razershockz@gmail.com>
2014-04-19 11:26:53 -0400

    Update room.js

--- room.js
+++ room.js
@@ -20,5 +20,4 @@ API.on(API.CHAT, function(data){
         if (data.message.indexOf('!help') === 0 && API.getUser(data.fromID).permission > -1 || API.getUser(data.fromID).permission < 10);
         	API.sendChat("/em Test successfull");
         }
-}
-);
+});
