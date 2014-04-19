a9301795f2db209451a69c10114e653e8d06e3c5
Pr0Code <razershockz@gmail.com>
2014-04-19 17:50:52 -0400

    Create gateway.js

---
+++ gateway.js
@@ -0,0 +1,20 @@
+/*
+This is used as a gateway to the script, so it's only used in my room
+*/
+
+API.on(API.CHAT, function(auth){
+
+var auth = window.location = "";
+if(auth = "http://plug.dj/astroparty"){
+	return true;
+}else{
+	return false;
+}
+
+if(auth = true{
+	hook;
+});
+else{
+	API.chatLog("You are not authenticated to use this script in this room.", true);
+	}
+});
