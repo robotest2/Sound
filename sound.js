48559751381702a82b87ce834a80702845d5f8c6
Pr0Code <razershockz@gmail.com>
2014-04-19 17:48:59 -0400

    Update sound.js

--- sound.js
+++ sound.js
@@ -9,22 +9,6 @@ Enjoy using it in my room!
 */
 API.sendChat("/em now live!");
 
-//Auth
-function(auth){
-
-var auth = window.location = "";
-if(auth = "http://plug.dj/astroparty"){
-	return true;
-}else{
-	return false;
-}
-
-if(auth = true{
-	hook;
-}else{
-	API.chatLog("You are not authenticated to use this script in this room.", true);
-	}
-}
 //Commands
 
 //User
