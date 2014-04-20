c03e789f92e312ab638d038c298fe38f81cdfccf
Pr0Code <razershockz@gmail.com>
2014-04-20 07:41:13 -0400

    Update gateway.js

--- gateway.js
+++ gateway.js
@@ -9,7 +9,7 @@ var hook = (function(){
 
 });
 
-var auth = window.location = "http://plug.dj/astroparty";
+var auth = window.location.assign = "http://plug.dj/astroparty";
 if(auth = true){
 	API.chatLog("Authentication Successful!");
         hook;
