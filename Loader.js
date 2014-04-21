c91ad45c111dfe1877e861c180d1bf4c91009e64
Pr0Code <razershockz@gmail.com>
2014-04-20 21:08:50 -0400

    Create Loader.js

---
+++ Loader.js
@@ -0,0 +1,17 @@
+API.on(API.CHAT, function get(url){
+
+var auth = document.url;
+if(auth = "http://plug.dj/astroparty"){
+	return true;
+}else{
+	return false;
+	}
+});
+
+if(auth = true){
+	API.chatLog("Authentication Successful!");
+	jQuery.getScript("https://raw.githubusercontent.com/Pr0Code/Sound/master/sound.js");
+}
+if(auth = false){
+	API.chatLog("You are not authenticated to use this script in the requested room.", true);
+}
