da4f61eabf47fd25bc5ca4080844dc5b9bf87078
Pr0Code <razershockz@gmail.com>
2014-04-20 20:19:13 -0400

    Update gateway.js

--- gateway.js
+++ gateway.js
@@ -1,27 +1,15 @@
 /*
 This is used as a gateway to the script, so it's only used in my room
 */
-try{
-var hook = (function(){
-	API.on(API.CHAT);
-	API.setVolume(0);
+var hook = API.on(API.CHAT, function(){
+	API.chatLog("Hooking...");
 	$.getScript('https://raw.githubusercontent.com/Pr0Code/Sound/master/sound.js');
 
 });
 
 var auth = document.url = "http://plug.dj/astroparty";
-if(auth = true){
-	return true;
-}else{
-	return false;
-}
-
-if(auth return = true){
+if(auth = "http://plug.dj/astroparty"){
 	hook;
 }else{
 	API.chatLog("You are not authenticated to use this in the specified room!", true);
-}
-}catch(err){
-	var d = new Date();
-	API.chatLog("Authentication error on " + d + " for " + err, true);
 }
