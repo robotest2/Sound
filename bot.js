36ed1d898c788c787e5b9d342c3352bc633df8e5
Grégory Gérard <mailperso.gregory@gmail.com>
2014-04-29 00:00:41 +0200

    Update bot.js
    TimeGuard

--- bot.js
+++ bot.js
@@ -188,19 +188,15 @@ API.chatLog("Loading file...");
 }
 }
 
-var l = API.getTimeRemaining();
 
 API.on(API.DJ_ADVANCE, function(){
+	var l = API.getTimeRemaining();
 	
-	if(options.overLimit == true){
-		if(l > 600000){
-			API.sendChat("/em Song is over the limit!");
-			API.moderateForceSkip();
-		}
-	}else{
-		if(l > 600000){
-			API.sendChat("/em This song is over the limit, but since overLimit is set to " + options.overLimit + ", I will do nothing!");
-		}
+	if(options.overLimit && l > 600000){
+		API.sendChat("/em Song is over the limit!");
+		API.moderateForceSkip();
+	}else if(!options.overLimit && l > 600000){
+		API.sendChat("/em This song is over the limit, but since overLimit is set to " + options.overLimit + ", I will do nothing!");
 	}
 
 });
