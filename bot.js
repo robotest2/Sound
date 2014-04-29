8c5d7feba63c6cbc9c27b6442cf799edea22fe72
Pr0Code <razershockz@gmail.com>
2014-04-29 19:56:20 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -314,7 +314,7 @@ var fightArray = [
 	" loves one-direction.",
 	" eats coconuts"];
 
-function userc(str, from, id, opt, data) { // Commands (WAYZ IS GOD)
+function userc(str, from, id, opt) { // Commands (WAYZ IS GOD)
 	var users = API.getUsers();
 	switch(str) {
 		case '!help':
