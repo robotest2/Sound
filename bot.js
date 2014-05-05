213842f056d85f27dc5ec917b5334c7d7cd15203
AstroShock <razershockz@gmail.com>
2014-05-04 20:37:04 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -853,8 +853,8 @@ function userc(str, from, fromid, chatid, opt) { // Commands (WAYZ IS GOD)
 					var crowd = API.getUsers();
 					for(var i in crowd){
 						if(crowd[i].username == opt){
-							API.sendChat("/em [" + from + "] Set " + crowd[i] + " as a Resident DJ!");
-							API.moderateSetRole(crowd[i].id, API.ROLE.RESIDENTDJ);
+							API.sendChat("/em [" + from + "] Set " + crowd[i].username + " as a Resident DJ!");
+							API.moderateSetRole(opt.id, API.ROLE.RESIDENTDJ);
 						}
 					}
 				}else{
