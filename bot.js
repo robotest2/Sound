1b741c363f20a907f6b68ee57e83e11f9c4a20c5
AstroShock <razershockz@gmail.com>
2014-05-07 20:30:51 -0400

    COMMANDS DONE CONVERTING (about time :p)

--- bot.js
+++ bot.js
@@ -491,8 +491,44 @@ function userc(str, from, fromid, chatid, opt) { // Commands (WAYZ IS GOD)
 	var users = API.getUsers();
 	switch(str) {
 		case '!help':
-				API.moderateDeleteChat(chatid);
-				API.sendChat("/em [" + from + "] Help commands: http://goo.gl/wfpGqc");
+			API.moderateDeleteChat(chatid);
+			API.sendChat("/em [" + from + "] Help commands: http://goo.gl/wfpGqc");
+			break;
+			
+		case '!theme':
+			API.moderateDeleteChat(chatid);
+			API.sendChat("/em [" + from + "] The theme is Electronic Dance Music");
+			break;
+			
+		case '!fight':
+			API.moderateDeleteChat(chatid);
+			var fm = str.substr(7).trim();
+			var fmsg = fm[1];
+			var fusers = API.getUsers();
+			for(var i in fusers){
+				if(fusers[i].username == fmsg){
+					var FR = Math.floor(Math.random() * fightArray.length);
+					API.sendChat("@" + fusers[i].username + ", " + from + " says: " + fightArray[FR]);
+				}else{
+					API.sendChat("/em [" + from + "] User not found!");
+				}
+			}
+			break;
+			
+		case '!cookie':
+			API.moderateDeleteChat(chatid);
+			var cm = str.substr(8).trim();
+			var cmsg = cm[1];
+			var cusr = API.getUsers();
+			for(var i in cusr){
+				if(cusr[i].username == cmsg){
+					var ca = Math.floor(Math.random() * cookieArray.length);
+					var co = Math.floor(Math.random() * outcome.length);
+					API.sendChat("@" + cusr[i].username + ", " + from + " gives you " + cookieArray[ca] + "! " + outcome[co]);
+				}else{
+					API.sendChat("/em [" + from + "] User not found!");
+				}
+			}
 			break;
 			
 		case '!link':
@@ -790,6 +826,17 @@ function userc(str, from, fromid, chatid, opt) { // Commands (WAYZ IS GOD)
 			}
 			break;
 			
+		case '!lockskip':
+			if(API.getUser(fromid).permission >= 2){
+				API.moderateDeleteChat(chatid);
+				API.sendChat("/em [" + from + " used lockskip]");
+				API.moderateLockWaitList(true, false);
+				API.moderateForceSkip();
+			}else{
+				API.sendChat("/em [" + from + "] No permission!");
+			}
+			break;
+			
 		case '!lock':
 			if(API.getUser(fromid).permission >= 2){
 				API.moderateDeleteChat(chatid);
@@ -962,358 +1009,6 @@ API.on(API.CHAT, function(data) {
 	if (userData[data.fromID].mute === true) API.moderateDeleteChat(data.chatID);
 });
 
-/*
-   _____                                          _     
-  / ____|                                        | |    
- | |     ___  _ __ ___  _ __ ___   __ _ _ __   __| |___ 
- | |    / _ \| '_ ` _ \| '_ ` _ \ / _` | '_ \ / _` / __|
- | |___| (_) | | | | | | | | | | | (_| | | | | (_| \__ \
-  \_____\___/|_| |_| |_|_| |_| |_|\__,_|_| |_|\__,_|___/
-                                                        
-                                                      
-*/
-//Spin Arrays
-/*
-var sa = []; //Initial !play array
-var se = []; //users that cannot lose
-var sar = Math.floor(Math.random() * sa.length); //Gets a random user that joined the game
-var sTime = Date.now();
-var aTime = sTime + sa.length;
-var rTime = Math.floor(Math.random() * aTime + 1000);
-//User
-	
-  API.on(API.CHAT, function(data){
-    
-    if(data.message == '!help'){
-           API.moderateDeleteChat(data.chatID);
-           API.sendChat("/em [" + data.from + " My commands can be found here: http://goo.gl/PzvBL8]");
-       
-       }
-    
-    if(data.message === '!spin'){
-    	API.moderateDeleteChat(data.chatID);
-    	API.sendChat("/em " + data.from + " requested a game of spin! Type !play to join the game!");
-    	for(var i in sar){
-    		sa.push(data.fromID);
-    	}
-    }
-    	if(data.message === '!play'){
-    		API.moderateDeleteChat(data.chatID);
-    		API.sendChat("/em " + data.from + " joined spin!");
-    		for(var i in sar){
-    			sa.push(data.fromID);
-    		}
-    	}
-    		if(data.message === '!start'){
-    			API.moderateDeleteChat(data.chatID);
-    			API.sendChat("/em " + data.from + " started spin!");
-    			setTimeout(function(){
-    				API.sendChat("/em Spinning the ball...");
-    			}, 1000);
-    			
-    			for(var i in sar){
-    			
-    			setTimeout(function(){
-    				API.sendChat("@" + sar.username + " you got the ball! Type !pass to pass it!");
-    			}, 2000);
-    			
-    			setTimeout(function(){
-    				API.sendChat("Oh no! @" + sar.username + " got thier brains blown out!");
-    				sa = [];
-    				se = [];
-    			}, rTime);
-    			}
-    		}
-    		if(data.message === '!pass'){
-    			API.moderateDeleteChat(data.chatID);
-    			API.sendChat("/em " + data.from + " passed the ball!");
-    			for(var i in sar){
-    				sa.pop(data.fromID);
-    			}
-    			for(var i in se){
-    				se.push(data.fromID);
-    			}
-    			setTimeout(function(){
-    				API.sendChat("@" + sar.username + " you got the ball! Type !pass to pass it!");
-    			}, 2000);
-    		}
-    
-    	if(data.message == '!god'){
-    		API.moderateDeleteChat(data.chatID);
-    		API.sendChat("/em [" + data.from + "] Credits: This was created by AstroShock, but helped by WayzRG (ProdTv) for some stuff! :D Thanks!");
-    	}
-    
-      if(data.message == '!fight'){
-        API.moderateDeleteChat(data.chatID);
-        var fightUser = API.getUsers();
-        var fightR = Math.floor(Math.random() * fightArray.length);
-        var userFR = Math.floor(Math.random() * fightUser.length);
-        API.sendChat("[" + data.from + "] @" + fightUser[userFR].username + fightArray[fightR]);
-      }
-      
-      if(data.message == '!staff'){ //credit: WayzRG
-        var isonline = []; 
-        API.moderateDeleteChat(data.chatID); 
-        var online = API.getStaff();
-        for(var i in online) {
-                isonline.push(online[i].username);
-        }
-        API.sendChat("/em [" + data.from + "] Staff that's online: [" + isonline.join(', ') + "]");
-        isonline = [];
-}
-
-      if(data.message == '!theme'){
-        API.moderateDeleteChat(data.chatID);
-        API.sendChat("/em [" + data.from + " The theme is Electronic Dance Music. (EDM)]");
-      }
-    
-      if(data.message == '!emoji'){
-        API.moderateDeleteChat(data.chatID);
-        API.sendChat("/em [" + data.from + " List of all emoji's here: http://www.emoji-cheat-sheet.com]");
-      }
-    
-      if(data.message == '!cookie'){
-        API.moderateDeleteChat(data.chatID);
-        var room = API.getUsers();
-        var cookieR = Math.floor(Math.random() * cookieArray.length);
-        var userR = Math.floor(Math.random() * room.length);
-        var outcomeR = Math.floor(Math.random() * outcome.length);
-        API.sendChat("[@" + room[userR].username + ", " + data.from + " gives you " + cookieArray[cookieR] + " cookie " + ", " + outcome[outcomeR] + "]");
-      }
-    
-      if(data.message == '!ba'){
-        API.moderateDeleteChat(data.chatID);
-        API.sendChat("/em [" + data.from + " Brand Ambassaadors (BA's) are PlugDJ's global moderators. More info here: http://blog.plug.dj/brand-ambassadors/]");
-      }
-    
-      if(data.message == '!link'){
-        if(API.getMedia().format == 1){
-          API.moderateDeleteChat(data.chatID);
-          API.sendChat("/em [" + data.from + " Link to current song: http://youtu.be/" + API.getMedia().cid + "]");
-        }else{
-          API.moderateDeleteChat(data.chatID);
-          var id = API.getMedia().cid;
-          SC.get('/tracks', { ids: id,}, function(tracks) {
-            API.sendChat("/em [" + data.from + " Link to current song: " + tracks[0].permalink_url + "]");
-          });
-        }
-      }
-    
-      if(data.message == '!ask'){
-        API.moderateDeleteChat(data.chatID);
-        var askR = Math.floor(Math.random() * askArray.length);
-        API.sendChat("/em [" + data.from + "] " + askArray[askR]);
-      }
-	
-	if(data.message.indexOf('!eta') !=-1){
-		API.moderateDeleteChat(data.chatID);
-		var a = data.message.split("@");
-		var y = a[1];
-		var b = API.getUsers();
-		for (var i in b) {
-			if(b[i].username == y) {
-				var c = API.getUser(b[i].id).wlIndex + 1;
-				var d = 5;
-				if(c == 1) {
-					var e = $("#now-playing-time").children('span').text();
-					API.sendChat("/em [" + data.from + "] ETA for " + y + " is " + e + " minutes.");
-				}
-				else if(c > 1) {
-					var f = Math.floor(c*d);
-					API.sendChat("/em [" + data.from + "] ETA for " + y + " is " + f + " minutes.");
-				}
-				else API.sendChat("/em [" + data.from + "] ETA for " + y + " is N/A minutes.");
-			}
-		}
-	}
-		
-    //Put more here soon
-	
-    //Bouncer
-        if(data.message == '!cycle' && API.getUser(data.fromID).permission >= 1){
-        	API.moderateDeleteChat(data.chatID);
-        	API.sendChat("/em [" + data.from + " used cycle]");
-        	var toggle = $(".cycle-toggle");
-        	if(toggle.hasClass("disabled")) {
-        		toggle.click();
-        	}else{
-        		toggle.click();
-        	}
-        }
-    
-    	if(data.message == '!settings' && API.getUser(data.fromID).permission >= 1){
-    		API.moderateDeleteChat(data.chatID);
-    		API.sendChat("/em [" + data.from + "] Settings | Auto Woot: " + options.woot + ", Announcement Message: " + options.announcementMsg + ", Log user Join: " + options.logUserJoin + ", AFK Remove: " + options.afkRemove + ".");
-    	}
-	if (data.message.indexOf("!mute") !=-1 && API.getUser(data.fromID).permission > 1) {
-		var msg = data.message.split("@");
-		var user = msg[1];
-		var users = API.getUsers();
-		for (var i in users) {
-			if (users[i].username == user) {
-				userData[users[i].id].mute = true;
-				API.sendChat("/me [" + data.from + "] used mute on: " + user);
-			}
-		}
-        API.moderateDeleteChat(data.chatID);
-	}
-	// if user muted
-    	if (userData[data.fromID].mute === true) API.moderateDeleteChat(data.chatID);
-	if (data.message.indexOf("!unmute") !=-1 && API.getUser(data.fromID).permission > 1) {
-		var msg = data.message.split("@");
-		var user = msg[1];
-		var users = API.getUsers();
-		if(userData[data.fromID].mute === true){
-			API.sendChat("/em [" + data.from + "] Tried unmuting themselves, but they're muted!");
-		}else{
-		for (var i in users) {
-			if (users[i].username == user) {
-				userData[users[i].id].mute = false;
-				API.sendChat("/me [" + data.from + "] used unmute on: " + user);
-				}
-			}
-			API.moderateDeleteChat(data.chatID);
-		}
-	}
-    
-	if (data.message.indexOf("!kick") !=-1 && API.getUser(data.fromID).permission >= 1 ) {
-		var messkick = data.message;
-		var splitkick = messkick.split("@");
-		var userskick = API.getUsers();
-		for(var i in userskick) {
-			if (userskick[i].username == splitkick[1]) {
-				var userkick = userskick[i].id;
-				API.sendChat("[" + data.from + "] @" + splitkick[1] + " You will be kicked in 10 seconds.")
-				setTimeout(function(){API.moderateBanUser(userkick, 1, API.BAN.HOUR)}, 10000);
-				setTimeout(function(){API.moderateUnbanUser(userkick)}, 15000);
-				setTimeout(function(){API.moderateUnbanUser(userkick)}, 18000);
-			}
-		}
-		API.moderateDeleteChat(data.chatID);
-	}
-        
-	if (data.message.indexOf('!ban') !=-1 && API.getUser(data.fromID).permission >= 1 ) {
-		API.moderateDeleteChat(data.chatID);
-		API.sendChat('/em [' + data.from + ' used ban]');
-                var messb = data.message;
-		var userb = messb.split("@");
-		var usersb = API.getUsers();
-		for(var b in usersb){
-			API.moderateBanUser(usersb[b].id);
-		}
-	}
-	
-        if(data.message.indexOf('!say') !=-1 && API.getUser(data.fromID).permission >= 1){
-        	API.moderateDeleteChat(data.chatID);
-        	var sayMsg = data.message.substr(5).trim();
-        	API.sendChat("/em [" + data.from + "] " + sayMsg);
-        }
-    
-    	if(data.message == '!lock' && API.getUser(data.fromID).permission >= 1){
-    		API.moderateDeleteChat(data.chatID);
-    		API.sendChat("/em [" + data.from + " used lock]");
-    		API.moderateLockWaitList(true);
-    	}
-    
-    	if(data.message == '!unlock' && API.getUser(data.fromID).permission >= 1){
-    		API.moderateDeleteChat(data.chatID);
-    		API.sendChat("/em [" + data.from + " used unlock]");
-    		API.moderateLockWaitList(false);
-    	}
-    
-    	if(data.message == '!lockskip' && API.getUser(data.fromID).permission >= 1){
-    		API.moderateDeleteChat(data.chatID);
-    		API.sendChat("/em [" + data.from + " used lockskip]");
-    		API.moderateLockWaitList(true);
-    		API.moderateForceSkip();
-    	}
-    
-    	if(data.message == '!wlclear' && API.getUser(data.fromID).permission >= 1){
-    		API.moderateDeleteChat(data.chatID);
-    		API.sendChat("/em [" + data.from + " used wlclear]");
-    		setTimeout(function(){
-    		API.moderateLockWaitList(true, true);
-    		}, 1000);
-    		setTimeout(function(){
-    			API.moderateLockWaitList(false);
-    		}, 2000);
-    	}
-    
-    	if(data.message == '!clear' && API.getUser(data.fromID).permission >= 1){
-    		API.moderateDeleteChat(data.chatID);
-		var messages = $('#chat-messages').children();
-		for (var i = 0; i < messages.length; i++) {
-			for (var j = 0; j < messages[i].classList.length; j++) {
-				if (messages[i].classList[j].indexOf('cid-') == 0) {
-					API.moderateDeleteChat(messages[i].classList[j].substr(4));
-					}
-				}
-			}
-			API.sendChat("/em [" + data.from + " used clear]");
-    		}
-    
-    	if(data.message == '!skip' && API.getUser(data.fromID).permission >= 1){
-    		API.moderateDeleteChat(data.chatID);
-    		API.sendChat("/em [" + data.from + " used skip]");
-    		API.moderateForceSkip();
-    	}
-        
-        //Manager +
-        
-        if(data.message == '!rdj' &&  API.getUser(data.fromID).permission >= 2){
-        	API.moderateDeleteChat(data.chatID);
-        	var messRdj = data.message;
-		var userRdj = messbRdj.split("@");
-		var usersRdj = API.getUsers();
-		for(var i in usersRdj){
-			API.sendChat("/em [" + data.from + "] Set " + userRdj + "'s permission to Resident DJ");
-			API.moderateSetRole(usersRdj[i].id, API.ROLE.RESIDENTDJ);
-		}
-        }
-        
-        if(data.message == '!bouncer' && API.getUser(data.fromID).permission >= 2){
-        	API.moderateDeleteChat(data.chatID);
-        	var messbo = data.message;
-		var userbo = messbo.split("@");
-		var usersbo = API.getUsers();
-		for(var i in usersbo){
-			API.sendChat("/em [" + data.from + "] Set " + userbo + "'s permission to Bouncer");
-			API.moderateSetRole(usersbo[i].id, API.ROLE.BOUNCER);
-		}
-        }
-        
-        //Co-Host +
-        
-        if(data.message == '!manager' && API.getUser(data.fromID).permission >= 4){
-        	API.moderateDeleteChat(data.chatID);
-        	var messm = data.message;
-		var userm = messm.split("@");
-		var usersm = API.getUsers();
-		for(var i in usersm){
-			API.sendChat("/em [" + data.from + "] Set " + userm + "'s permission to Manager");
-			API.moderateSetRole(usersm[i].id, API.ROLE.MANAGER);
-		}
-        }
-        
-        //Host
-        
-        if(data.message == '!cohost' && API.getUser(data.fromID).permission >= 4){
-        	API.moderateDeleteChat(data.chatID);
-        	var messCh = data.message;
-		var userCh = messCh.split("@");
-		var m = userCh[1];
-		var usersCh = API.getUsers();
-		for(var i in usersCh){
-			if (usersCh == m){
-			API.sendChat("/em [" + data.from + "] Set " + m + "'s permission to Co-Host");
-			API.moderateSetRole(m[i].id, API.ROLE.COHOST);
-			}else{
-				API.sendChat("/em [" + data.from + "] User not found");
-			}
-		}
-        }
-  }); // end of commands
-*/
 	}catch(err){
     	var d = new Date();
     	API.sendChat("/em An error has occurred on " + d + " for " + err + " on line " + err.lineNumber);
@@ -1323,7 +1018,3 @@ var rTime = Math.floor(Math.random() * aTime + 1000);
 else API.chatLog('This script works only in plug.dj/astroparty', true);
     
 //End of script. No seriously, there's nothing below me
-
-//except this
-
-API.on(API.addEventListener, readChat);
