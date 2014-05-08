8b1aab77f1cfefae953d184a79b7fe68b70905e3
AstroShock <razershockz@gmail.com>
2014-05-07 20:40:23 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -56,11 +56,6 @@ if(location.pathname == '/astroparty/') {
 
 var msgArray = [
 	"Welcome to the AstroShock plug.dj room!",
-	"My commands are being re-written, so not all commands work!",
-	"My commands are being re-written, so not all commands work!",
-	"My commands are being re-written, so not all commands work!",
-	"My commands are being re-written, so not all commands work!",
-	"My commands are being re-written, so not all commands work!",
 	"Make sure to help out new users!",
 	"Need help? Type !help for a list of commands",
 	"This script is protected with an authentication system!",
